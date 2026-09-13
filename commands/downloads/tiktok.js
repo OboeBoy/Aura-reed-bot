0218689d10afbd3e8de64021900065859cc5f70a
JerielWeb <jerielweb@outlook.es>
2026-09-13 13:38:57 -0600

    Actualiza la URL de TikTok para usar el nombre completo del autor en lugar del apodo

--- commands/downloads/tiktok.js
+++ commands/downloads/tiktok.js
@@ -77,7 +77,7 @@ async function DL_TIKTOK(input) {
         collect: r.stats?.download || formatter(r.collect_count || 0),
         comments: r.stats?.comment || formatter(r.comment_count || 0),
         time: r.taken_at || dateCreate(r.create_time || 0),
-        tk_url: `https://www.tiktok.com/@${r.author?.nickname}/video/${r.id}`,
+        tk_url: `https://www.tiktok.com/@${r.author?.fullname}/video/${r.id}`,
       };
     }
     throw new Error("No se pudieron extraer los datos del video con TikWM.");
